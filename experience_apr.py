2d44c4b810e9e1246088c46cb0fe12d446e7d67e
Beucler_Jeremie <jeremie.beucler>
2019-11-20 13:45:04 +0100

    changed

--- experience_apr.py
+++ experience_apr.py
@@ -1,3 +1,5 @@
+"""Expérience de discrimination de visages à 4 conditions à l'aide d'Expyriment dont les résultats par sujet sont enregistrés dans un dossier 'data'"""
+
 # -*- coding: utf-8 -*-
 import random
 import expyriment
@@ -17,7 +19,7 @@ while counter < 1:
         counter +=1
     else:
         counter = counter
-#counterbalance the laterality
+#permet de changer une fois sur deux la touche 'pareil' et la touche 'différent' pour contrebalancer les effets de latéralité
 
 exp = expyriment.design.Experiment(name="Perception des visages")
 expyriment.control.initialize(exp)
@@ -39,7 +41,7 @@ compte_a_reb_4 = expyriment.stimuli.TextLine(text="2", text_size=45)
 list_rebours.append(compte_a_reb_4)
 compte_a_reb_5 = expyriment.stimuli.TextLine(text="1", text_size=45)
 list_rebours.append(compte_a_reb_5)
-#création des textes précédant l'expérience
+#création des textes (consignes, etc.) précédant l'expérience
 
 upr_tot = []
 upd_tot = []
@@ -49,7 +51,7 @@ same = []
 diff = []
 l_glob = []
 list_stim = []
-#4 listes pour les 4 conditions + 2 selon pareil/différent + 2 pour la présentation de tous les stimuli
+#création de 4 listes pour les 4 conditions + 2 selon pareil/différent + 2 pour la présentation de tous les stimuli
 
 for i in range(1, 5):
 	l_glob.append('P' + str(i) + 'FDAT' + '.jpg')
@@ -68,14 +70,14 @@ for i in range(1, 5):
 	l_glob.append('P' + str(i) + 'FSEK' + '.jpg')
 	l_glob.append('P' + str(i) + 'MSET' + '.jpg')
 	l_glob.append('P' + str(i) + 'MSEK' + '.jpg')
-#generate the filenames of the pictures according to the factor sex M/F, same/different S/D, upright/upside-down A/E, and K/T partiel/total 
-#and put them in a list (so the pictures need to have precise names for the program to work)
+#créé les noms des fichiers par rapport au facteur sexe (M/F), pareil/différent (S/D), endroit/envers (A/E), et floutage des yeux partiel/total(K/T)
+#donc nécessite que les photos aient des noms précis pour fonctionner
 
 upr_l = []
 upd_l = []
 part_l = []
 tot_l = []
-#sous-listes pour distribuer dans les 4 listes des 4 conditions (voir block ci-dessous)
+#création de 4 sous-listes pour distribuer dans les 4 listes des 4 conditions (voir block ci-dessous)
 
 y = 0
 while y < len(l_glob):
@@ -105,28 +107,20 @@ while y < len(l_glob):
 	elif list_stim[y] in upd_l and list_stim[y] in part_l:
 		upd_part.append(list_stim[y])
 	y +=1
-	#distribue le stim selon les différents facteurs: 4 conditions
+	#distribue le stim dans les 4 listes selon les différents facteurs (4 conditions: upr_tot, etc.)
 
 random.shuffle(list_stim)
 list_train = list_stim[0: 10]
-#créé une liste d'entraînement de 10 stim au hasard
+#créé une liste d'entraînement de 10 stim au hasard parmi les stimuli
 random.shuffle(list_stim)
 
 
 cross = expyriment.stimuli.FixCross(colour=(255, 255, 255))
 cross.preload()
-#croix de fixation
-
-upr_tot_rt = 0
-upr_tot_prf = 0
-upd_tot_rt = 0
-upd_tot_prf = 0
-upr_part_rt = 0
-upr_part_prf = 0
-upd_part_rt = 0
-upd_part_prf = 0
+#importe la croix de fixation
 
 exp.data_variable_names = ["Condition", "Correct", "RT"]
+#créé les différentes colonnes du fichier 'data'
 
 expyriment.control.start()
 
@@ -136,6 +130,7 @@ text_2.present()
 exp.keyboard.wait()
 text_3.present()
 exp.keyboard.wait()
+#donne les consignes au sujet
 
 for elt in list_train:
 	cross.present()
@@ -143,7 +138,7 @@ for elt in list_train:
 	elt.present()
 	exp.keyboard.wait([expyriment.misc.constants.K_l,
 								expyriment.misc.constants.K_s])
-#liste d'entraînement de 10 stimuli dont les données ne sont pas prises en compte
+#liste d'entraînement de 10 stimuli dont les données ne sont pas prises en compte dans les résultats
 
 text_4.present()
 exp.clock.wait(3500)
@@ -162,89 +157,67 @@ for elt in list_stim:
 			if elt in same:
 				if key == nb_key_same:
 					exp.data.add(["upr_tot", "correct", rt])
-					upr_tot_prf += 1
-					upr_tot_rt += rt
+
 				else:
 					exp.data.add(["upr_tot", "false", rt])
-					upr_tot_rt += rt
+					
 			if elt in diff:
 				if key == nb_key_same:
 					exp.data.add(["upr_tot", "false", rt])
-					upr_tot_rt += rt
+					
 				else:
 					exp.data.add(["upr_tot", "correct", rt])
-					upr_tot_prf += 1
-					upr_tot_rt += rt
+					
 		
 		if elt in upr_part:
 			if elt in same:
 				if key == nb_key_same:
 					exp.data.add(["upr_part", "correct", rt])
-					upr_part_prf += 1
-					upr_part_rt += rt
+					
 				else:
 					exp.data.add(["upr_part", "false", rt])
-					upr_part_rt += rt
+					
 			if elt in diff:
 				if key == nb_key_same:
 					exp.data.add(["upr_part", "false", rt])
-					upr_part_rt += rt
+					
 				else:
 					exp.data.add(["upr_part", "correct", rt])
-					upr_part_prf += 1
-					upr_part_rt += rt
+					
 					
 		if elt in upd_tot:
 			if elt in same:
 				if key == nb_key_same:
 					exp.data.add(["upd_tot", "correct", rt])
-					upd_tot_prf += 1
-					upd_tot_rt += rt
+					
 				else:
 					exp.data.add(["upd_tot", "false", rt])
-					upd_tot_rt += rt
+					
 			if elt in diff:
 				if key == nb_key_same:
 					exp.data.add(["upd_tot", "false", rt])
-					upd_tot_rt += rt
+					
 				else:
 					exp.data.add(["upd_tot", "correct", rt])
-					upd_tot_prf += 1
-					upd_tot_rt += rt
+					
 		
 		if elt in upd_part:
 			if elt in same:
 				if key == nb_key_same:
 					exp.data.add(["upd_part", "correct", rt])
-					upd_part_prf += 1
-					upd_part_rt += rt
+					
 				else:
 					exp.data.add(["upd_part", "false", rt])
-					upd_part_rt += rt
+
 			if elt in diff:
 				if key == nb_key_same:
 					exp.data.add(["upd_part", "false", rt])
-					upd_part_rt += rt
+					
 				else:
 					exp.data.add(["upd_part", "correct", rt])
-					upd_part_prf += 1
-					upd_part_rt += rt
+#selon la condition dans laquelle se trouve le stimulus, enregistre si sa réponse est correcte ainsi que son temps de réaction dans un fichier xpd dans le dossier 'data'
 			
 expyriment.control.end(goodbye_text="Merci pour votre participation!")
 
-nb_tot_stim = len(list_stim)
-
-mean_PRF_upr_tot = str(round((upr_tot_prf/(nb_tot_stim/4)) * 100))
-mean_PRF_upr_part = str(round((upr_part_prf/(nb_tot_stim/4)) * 100))
-mean_PRF_upd_tot = str(round((upd_tot_prf/(nb_tot_stim/4)) * 100))
-mean_PRF_upd_part = str(round((upd_part_prf/(nb_tot_stim/4)) * 100))
-mean_RT_upr_tot = str(round((upr_tot_rt/(nb_tot_stim/4))))
-mean_RT_upr_part = str(round((upr_part_rt/(nb_tot_stim/4))))
-mean_RT_upd_tot = str(round((upd_tot_rt/(nb_tot_stim/4))))
-mean_RT_upd_part = str(round((upd_part_rt/(nb_tot_stim/4))))
-
-resultsFile = open('results.txt', 'a')
-resultsFile.write(mean_PRF_upr_tot + ',' + mean_PRF_upr_part + ',' + mean_PRF_upd_tot + ',' + mean_PRF_upd_part + ',' + mean_RT_upr_tot + ',' + mean_RT_upr_part + ',' + mean_RT_upd_tot + ',' + mean_RT_upd_part + '\n')
-resultsFile.close()
 
 
